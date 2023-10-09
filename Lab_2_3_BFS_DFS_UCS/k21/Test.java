6c4a2cf3811670f864fd1f7c723641e9c9f55a3e
vutran <vutran08012k3>
2023-10-09 20:51:05 +0700

    Add Lab2_bai1

---
+++ Lab_2_3_BFS_DFS_UCS/k21/Test.java
@@ -0,0 +1,31 @@
+package k21;
+
+public class Test {
+	public static void main(String[] args) {
+		Node nodeS = new Node("S");
+		Node nodeA = new Node("A");
+		Node nodeB = new Node("B");
+		Node nodeC = new Node("C");
+		Node nodeD = new Node("D");
+		Node nodeE = new Node("E");
+		Node nodeF = new Node("F");
+		Node nodeG = new Node("G");
+		Node nodeH = new Node("H");
+		nodeS.addEdge(nodeA, 5);
+		nodeS.addEdge(nodeB, 2);
+		nodeS.addEdge(nodeC, 4);
+		nodeA.addEdge(nodeD, 9);
+		nodeA.addEdge(nodeE, 4);
+		nodeB.addEdge(nodeG, 6);
+		nodeC.addEdge(nodeF, 2);
+		nodeD.addEdge(nodeH, 7);
+		nodeE.addEdge(nodeG, 6);
+		nodeF.addEdge(nodeG, 1);
+		ISearchAlgo algo1 = new BFSGraphSearchAIgo();
+		Node result = algo1.execute(nodeS,"A", "G");
+		
+//		ISearchAlgo algo2 = new DFSGraphSearchAIgo();
+//		Node result = algo2.execute(nodeS,"A","G");
+		System.out.println(NodeUtils.printPath(result));
+	}
+}
